98e750537ad0b77a4519440a04e8191ec802dbba
bek001rasulov <tarhushrasulov@gmail.com>
2023-11-09 13:14:18 +0500

    28 urok getting data from coingecko

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
+import {useAppDispatch, useAppSelector} from "../../utils/hook";
+import {getFavoriteAssets} from "../../store/thunks/assets";
 
 const Home = () => {
+    const favoriteAssets = useAppSelector(state => state.assets.favoriteAssets)
+    const dispatch = useAppDispatch()
+    const fetchDataRef = useRef(false)
+
+    const favoriteAssetName = ['bitcoin', 'ethereum']
+
+    const fetchData = (data: string[]) => {
+        data.forEach((item) => {
+            dispatch(getFavoriteAssets(item))
+        })
+    }
+
+    console.log('ASSETS', favoriteAssets)
+
+    useEffect(() => {
+        if (fetchDataRef.current) return
+        fetchDataRef.current = true
+        fetchData(favoriteAssetName)
+    }, [])
+
     return (
         <div>
             <h1>This is a Homepage</h1>
