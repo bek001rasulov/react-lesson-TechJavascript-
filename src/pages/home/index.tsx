fe02776ab460bd58456e26767ab56c4c6b9f7f7b
bek001rasulov <tarhushrasulov@gmail.com>
2023-11-10 12:36:12 +0500

    29 urok create home component with crypto cards

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,32 +1,58 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useCallback, useEffect, useMemo, useRef} from 'react';
 import {useAppDispatch, useAppSelector} from "../../utils/hook";
 import {getFavoriteAssets} from "../../store/thunks/assets";
+import {Box, Grid} from "@mui/material";
+import {useStyles} from "./styles";
 
 const Home = () => {
-    const favoriteAssets = useAppSelector(state => state.assets.favoriteAssets)
+    const favoriteAssets: any[] = useAppSelector(state => state.assets.favoriteAssets)
     const dispatch = useAppDispatch()
     const fetchDataRef = useRef(false)
+    const classes = useStyles()
 
-    const favoriteAssetName = ['bitcoin', 'ethereum']
+    const favoriteAssetName = useMemo(() => ['bitcoin', 'ethereum'], [])
+    const filteredArray = favoriteAssets.filter((value, index, self) => index === self.findIndex((t) => t.name === value.name))
 
-    const fetchData = (data: string[]) => {
+    const fetchData = useCallback((data: string[]) => {
         data.forEach((item) => {
             dispatch(getFavoriteAssets(item))
         })
-    }
-
-    console.log('ASSETS', favoriteAssets)
+    }, [dispatch])
 
     useEffect(() => {
         if (fetchDataRef.current) return
         fetchDataRef.current = true
         fetchData(favoriteAssetName)
-    }, [])
+    }, [favoriteAssetName, fetchData])
+
+    const renderFavoriteBlock = filteredArray.map((item: any) => {
+        const currentPrice = item.data.prices[0]
+        const currentCap = item.data.market_caps[0]
+        return (
+            <Grid item lg={6} md={6} xs={12} key={item.name}>
+                <Grid container className={classes.topCardItem}>
+                    <Grid item lg={6} md={6} xs={12}>
+                        <h3 className={classes.assetName}>{item.name}</h3>
+                        <div className={classes.itemDetails}>
+                            <h3 className={classes.cardPrice}>${currentPrice[1].toFixed(2)}</h3>
+                            <p className={classes.cardCapitalize}>${currentCap[1].toFixed(0)}</p>
+                        </div>
+
+                    </Grid>
+                    <Grid item lg={6} md={6} xs={12}>
+                        <h5>Chart</h5>
+                    </Grid>
+                </Grid>
+            </Grid>
+        )
+    })
 
     return (
-        <div>
-            <h1>This is a Homepage</h1>
-        </div>
+        <Box className={classes.root}>
+            <Grid container spacing={2}>
+                {renderFavoriteBlock}
+            </Grid>
+        </Box>
     );
 };
 
